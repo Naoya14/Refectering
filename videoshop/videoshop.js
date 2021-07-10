350d9bc607f232690ca4e55ed4a3a982e86dc08e
Naoya Tabata <naoya1059@gmail.com>
2021-07-10 18:31:44 +0900

    Updated: change variable pref => aperformance

--- videoshop/videoshop.js
+++ videoshop/videoshop.js
@@ -25,23 +25,23 @@ function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(pref, play) {
+function amountFor(aperformance, play) {
   let result = 0;
   switch(play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30)
+      if (aperformance.audience > 30)
       {
-        result += 1000 * (pref.audience - 30);
+        result += 1000 * (aperformance.audience - 30);
       }
       break;
     case " comedy":
       result = 30000;
-      if (pref.audience > 20)
+      if (aperformance.audience > 20)
       {
-        this += 10000 + 500 * (pref.audience - 20);
+        this += 10000 + 500 * (aperformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aperformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
